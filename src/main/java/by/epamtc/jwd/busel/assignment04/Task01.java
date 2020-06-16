72d7a4d9d9e34eb1db1fc1e40a1bf52e92da44f6
Siarhei Busiel <sju.busel@gmail.com>
2020-06-16 11:46:03 +0300

    Add complete Task01

---
+++ src/main/java/by/epamtc/jwd/busel/assignment04/Task01.java
@@ -0,0 +1,53 @@
+package by.epamtc.jwd.busel.assignment04;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Task01 {
+    private static int getPositiveIntegerFromConsole() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            if (scanner.hasNextInt()) {
+                int number = scanner.nextInt();
+                if (number > 0 && number % 2 == 0) {
+                    return number;
+                } else {
+                    System.out.printf("Non-positive or/and even number" +
+                            " \"%d\"\n", number);
+                }
+            } else {
+                System.out.printf("Invalid input \"%s\"\n", scanner.next());
+            }
+        }
+    }
+
+    private static int[][] createSquareArray(int size) {
+        int[][] array = new int[size][size];
+        int initialElement = 1;
+        for (int i = 0; i < array.length; i++) {
+            boolean isAscending = ((i % 2) == 0);
+            for (int j = 0; j < array[i].length; j++) {
+                array[i][j] = isAscending ? initialElement++ : --initialElement;
+            }
+        }
+        return array;
+    }
+
+    private static void printSquareArray(int[][] array) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int[] element : array) {
+            stringBuilder.append(Arrays.toString(element)).append("\n");
+        }
+        System.out.println(new String(stringBuilder));
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Please, insert any positive even integer, which " +
+                "will be \"N\"");
+        int n = getPositiveIntegerFromConsole();
+        int[][] array = createSquareArray(n);
+        System.out.println(Arrays.deepToString(array));
+        printSquareArray(array);
+
+    }
+}
